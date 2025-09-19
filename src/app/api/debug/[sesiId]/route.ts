9730816c4f991461fc0e3082f523c74473ffc475
ppgcengkareng-apps <ppg.cengkareng@gmail.com>
2025-09-19 11:35:44 +0700

    Add files via upload

--- src/app/api/debug/[sesiId]/route.ts
+++ src/app/api/debug/[sesiId]/route.ts
@@ -30,6 +30,6 @@ export async function GET(
     })
 
   } catch (error) {
-    return NextResponse.json({ error: error.message }, { status: 500 })
+    return NextResponse.json({ error: String(error) }, { status: 500 })
   }
 }
